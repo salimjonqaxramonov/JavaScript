7584b534a572a536d896a3d20bc38ce93ea8830d
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-08 18:02:40 +0500

    chenged

--- 1-dars/index.js
+++ 1-dars/index.js
@@ -3,3 +3,16 @@ console.log("hi github")
 // 1. git init 
 
 // 2. git remote add orign https://github.com/salimjonqaxramonov/JavaScript.git
+
+// 3. git add . 
+
+// 4. git commit -m "github  buyruqlari"
+
+// 5. git config  --global user.email "salomjn@gmail.com"
+
+// 6. git config  --global user.name "salomjn@gmail.com"
+
+// 7. git push origin master
+
+//==================================================
+
