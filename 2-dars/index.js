1d7c6b5a53a0ec628543618e9ceb0cb04501906f
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-13 20:27:11 +0500

    Variables

---
+++ 2-dars/index.js
@@ -0,0 +1,66 @@
+// console.log("Hi JavaScript");
+// console.log("Hi Css!");
+// console.log("Hi Html!");
+
+// console.log("Shoxjahon1");
+// console.log("Shoxjahon2");
+// console.log("Shoxjahon3");
+
+// var matn = "suwshdjksahdkldkskduoqweijdwqijdiklsjlidjiwqddlqwj"
+
+// let matn = "suwshdjksahdkldkskduoqweijdwqijdiklsjlidjiwqddlqwj"
+
+// const matn = "suwshdjksahdkldkskduoqweijdwqijdiklsjlidjiwqddlqwj"
+
+// var, let, coonst
+
+// console.log(matn);
+
+// console.log(matn);
+
+// var name = "Eshmat";
+
+// var name = "Toshmat";
+
+// console.log(name);
+
+// ==============================
+
+// let phone = 998901112233;
+
+// let phone = 998999998877;
+
+// console.log(phone);
+
+// ======================================================================
+
+// const name = "eshmat";
+
+// const name = "Toshmat";
+
+// name = "Toshmat"
+
+// console.log(name);
+
+
+//================================================
+
+// js nima - saytning tili 
+
+// single Theard nima - 1ta ishga etbor berishi 
+
+// veribles nima - ozgaruvchi
+
+// Veriables necha xil usulda declare(e'lon) qilinadi ? - 3 xil usulda 1.var 2.let 3.const
+
+// Variables - var, let, const - farqlari ? 
+
+// var - 1ta ozgaruvchi berib 2marta ishlatsez boladi
+
+// let - 1ta ozgaruvchini qaytarb yozsez ogohlantiradi
+
+// const - ozgartirilmaydi 1ta ozgaruvchi qat'iy
+
+// Veriables nomlash usullari:
+
+// camelCase / snake_case / PascalCase / UPPER_CASE
