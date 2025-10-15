407ab019f3237eef6a0a4a6adea74000a7d79793
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-15 17:58:58 +0500

    3-dars

---
+++ 3-dars/index.js
@@ -0,0 +1,152 @@
+// var name = "eshmat"
+
+// console.log (name)
+
+// var age = "18"
+
+//  console.log (age)
+
+// var data = "100"
+
+//  console.log (data)
+
+// var name = "toshmat"
+
+// console.log (name)
+
+// var a = "gafur"
+
+// console.log (a)
+
+//  var b = "telefon"
+
+//  console.log (b)
+
+// var d = "televizr"
+
+//  console.log (d)
+
+// var e = "tv"
+
+//  console.log (e)
+
+// let f = "comp"
+
+// console.log (f)
+
+// let r = "comment"
+
+// console.log (r)
+
+// let g = "console"
+
+// console.log (g)
+
+// let h = "arab"
+
+// console.log (h)
+
+// let i = "anor"
+
+// console.log (i)
+
+// let j = "pul"
+
+// console.log (j)
+
+// let meva = "olma"
+
+// console.log (meva)
+
+// let raqam = "sinf"
+
+// console.log(raqam)
+
+// let raqam2 = "9"
+
+// console.log (raqam2)
+
+// let raqam3 = "10"
+
+// console.log (raqam3)
+
+// const qatiy = "100%"
+
+// console.log (qatiy)
+
+// const ozgarmas = "200%"
+
+// console.log (ozgarmas)
+
+// const ozgaruvchi = "2"
+
+// console.log (ozgaruvchi)
+
+// const ozgar = "3"
+
+// console.log (ozgar)
+
+// const ozgardi = "6"
+
+// console.log (ozgardi)
+
+// const ozgaradi = "7"
+
+// console.log (ozgaradi)
+
+// const ozgarmoqchi = "8"
+
+// console.log (ozgarmoqchi)
+
+// const ozgartrldi = "9"
+
+// console.log (ozgartrldi)
+
+// const ozga = "10"
+
+// console.log (ozga)
+
+// alert("salom")
+
+// let salom = alert
+
+// console.log(salom)
+
+// prompt("qayerlisiz")
+
+// let malumot = prompt("qayerlisiz")
+
+// console.log (malumot)
+
+// confirm (ozbekistonlikmisiz)
+
+// let confirim = prompt
+
+// console.log (confirim)
+
+// let yil = prompt("yilizni kiriting")
+
+// console.log(2025-yil)
+
+let year = prompt("O'zingizni yilizni kiriting")
+
+let one_age = 2025-year
+
+
+let year2 = prompt("otangizni yilini kiriting")
+
+let two_age = 2025-year2
+
+let year3 = prompt("onangizni yilini kiriting")
+
+let three_age = 2025-year3
+
+let year4 = prompt("singlingizni yilini kiriting")
+
+let four_age = 2025-year4
+
+let year5 = prompt("ukangizni yilini kiriting")
+
+let five_age = 2025-year5
+
+console.log ( (one_age+two_age+three_age+four_age+five_age) / 5);
