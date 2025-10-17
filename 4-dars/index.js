b2438b5d3210f875a138c772f421e6f07ba8498b
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-17 17:35:40 +0500

    4-dars

---
+++ 4-dars/index.js
@@ -0,0 +1,66 @@
+// 2--dars
+
+// let name ="Ali";
+
+// let age = 20;
+
+// let isMerrid = false 
+
+//Data type - ma'lumot turi 
+
+// 1. Primitive :
+
+// 1.1 - string : "Ali", '392829', `salom2025`
+
+// 1.2 - number : +-2^53, 1, 2, 3, 4, 5, 6, 10,3
+
+// 1.3 - bigint : 9007199254740993n = 2 ** 53 +1
+
+// 1.4 - boolen : true, false 
+
+// 1.5 - symbol : bir - biriga oxshamagan data yasab beradi.
+
+// 1.6 - null   : yo'q ma'lumot
+
+// 1.7 - undefined : yo'q ma'lumot
+
+// let ism = "Ali";
+
+// console.log(ism);
+
+// ism = null;
+
+// console.log(ism);
+
+//=========================================================
+
+// 2. Non Primetive
+
+// 2.1 - object : {key: Value, key2: value2}
+
+// let data = {name: "Ali", age:20};
+
+// 2.2 - Array : [1, "Ali", true, false. null, undefined,[2,3]]
+
+// 2.3 - Function :
+
+// 1 function xisobla(){}
+
+// 2. let xisobla2 = function(){}
+
+// 3, let xisobla3 = ()=>{}
+
+//==============================================================
+
+// let name = "Eshmat";
+
+// console.log (typeof name, name)
+
+// let person = {
+//  name: "Eshmat",
+//  age: 20,
+//  address: "Kosonsoy, katta masjid",
+// };
+
+
+
