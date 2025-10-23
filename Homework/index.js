46f8f4979d065f564b843737245034de928c214e
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-23 19:38:37 +0500

    6-dars

---
+++ Homework/index.js
@@ -0,0 +1,100 @@
+let son1 = 8;
+
+let son2 = 8
+
+console.log(son1+son2);
+
+console.log(son1-son2);
+
+console.log(son1*son2);
+
+console.log(son1/son2);
+
+let son3 = 4;
+
+let son4 = 2
+
+console.log(son3+son4);
+
+console.log(son3-son4);
+
+console.log(son3*son4);
+
+console.log(son3/son4);
+
+let son5 = 5;
+
+let son6 = 5
+
+console.log(son5+son6);
+
+console.log(son5-son6);
+
+console.log(son5*son6);
+
+console.log(son5/son6);
+
+let son7 = 9;
+
+let son8 = 3
+
+console.log(son7+son8);
+
+console.log(son7-son8);
+
+console.log(son7*son8);
+
+console.log(son7/son8);
+
+let a = 889;
+
+console.log(++a);
+console.log(a++);
+console.log(a--);
+console.log(--a);
+console.log(++a);
+console.log(++a);
+console.log(a);
+
+let b = 889;
+
+console.log(--b);
+console.log(b++);
+console.log(b--);
+console.log(--b);
+console.log(++b);
+console.log(--b);
+console.log(a);
+
+console.log(4**3);
+
+console.log(8**2);
+
+console.log(5**4);
+
+console.log(6**3);
+
+console.log("5" == 5);
+
+console.log("true" === true);
+
+console.log(undefined === null);
+
+
+console.log(7 > 4);
+
+console.log(6 < 3);
+
+console.log(5 <= "9");
+
+console.log(7 >= "8");
+
+console.log(true || false);
+
+console.log(null || false || " ");
+
+console.log(0 || false || "0" , "javob"); 
+
+console.log("null" || "NaN" || "0");
+
+
