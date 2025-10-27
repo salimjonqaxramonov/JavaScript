b19de61a9c39c2e4d8fcaf253151424c93c57a1b
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-27 17:50:09 +0500

    7-dars

--- 8-dars/index.js
+++ 8-dars/index.js
@@ -1,7 +1,78 @@
-let number = prompt("son kiriting")
+// let number = prompt("son kiriting")
+// if(n === 0){
+//     console.log("Bu son juft ham emas, toq ham emas");
+    
+// }
+// else if(number % 2 ){
+//     console.log("toq son");  
+// }else{
+//     console.log("juft son");   
+// }
 
-if(number % 2 ){
-    console.log("toq son");  
-}else{
-    console.log("juft  son");   
+// // Switch 
+
+// let name = prompt()
+
+// switch (name) {
+//     case "Eshmat":
+//       console.log("ha bu name eshmatga teng.");
+//       return;
+//     case "Toshmat":
+//       console.log("ha bu name Toshmatga teng.");
+//       return;
+//     case "ali":
+//       console.log("ha bu name Aliga teng.");
+//       return;
+//       default:
+//         console.log("Bu name bizda mavjud emas");      
+// }
+
+// let number = prompt("son kiriting")
+
+// switch (number % 2) {
+//     case 1 :
+//         console.log(number * 20, "Bu toq son edi");
+//         break;
+//     case 0 :
+//         console.log(number * 10, "Bu son juft edi");
+//         break;
+//     default:
+//         console.log("bu son xato kritilgan");      
+// }
+
+// let age = prompt("son kiriting")
+
+// switch (age >= 18) {
+//     case true :
+//         console.log("kattalar");
+//         break;
+//     case false :
+//         console.log("kichkinalar");
+//         break;
+//     default:
+//         console.log("notogri son kiritingiz");
+// }
+
+//====================================================
+
+//?, :
+
+// let n = prompt("son kiriting")
+
+// n % 2 ? console.log("bu toq son") : console.log("bu juft son");
+let a = 5;
+let b = 10;
+let c = 30;
+let number = a+b+c
+switch (number%2) {
+    case 1:
+        console.log("toq son");
+        break;
+    case 0 :
+        console.log("juft son");
+        
+    default:
+        console.log("bu son emas");
+        
+       
 }
