40cc394eddef102a0c7f6c4c68e43831d72980ab
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-29 16:29:12 +0500

    8-dars

---
+++ Homework 3/index.js
@@ -0,0 +1,34 @@
+if (browser === "Edge") {
+ console.log("You've got the Edge!");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  console.log("Okay we support these browsers too");
+  
+} else {
+  console.log("We hope that this page looks ok!");
+  
+}
+
+
+let message = prompt("habar kiriting");
+
+message =
+  login == "Employee"
+    ? console.log("Hello")
+    : login == "Director"
+    ? console.log("Greetings")
+    : login == ""
+    ? console.log("No login")
+    : "";
+
+let result = prompt("son kiriting");
+
+let a = 2
+let b = 3
+
+result = a + b < 4 ? "Below" : "Over";
+
