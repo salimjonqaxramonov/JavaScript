eb51d3612fcd090416a93375cc04b37efec4ebce
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-10-31 16:43:05 +0500

    9-dars

---
+++ Homework 4/index.js
@@ -0,0 +1,25 @@
+// let number = 20;
+
+// for (let i = 2; i <= number; i++) {
+//   let tup= true;
+
+//   for (let bolnuvchi = 2; bolnuvchi < i; bolnuvchi++) {
+//     if (i % j === 0) {
+//       tup = false;
+//       break;
+//     }
+//   }
+
+//   if (tup) {
+//     console.log(i);
+//   }
+// }
+
+
+for( ;true; ){
+    let n = prompt("son kiriting")
+    if(n > 100 || n === null){
+        console.log(n);
+        break
+    }
+}
