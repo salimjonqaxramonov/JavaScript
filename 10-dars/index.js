842d424e38a28de02e9697df7915ac7f9cb61dee
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-11-03 16:12:09 +0500

    10-dars

---
+++ 10-dars/index.js
@@ -0,0 +1,52 @@
+// for( ;true; ){
+//     let n = prompt("son kiriting")
+//     if(n > 100 || n === null){
+//         console.log(n);
+//         break
+//     }
+// }
+
+// while
+
+// while (1){
+//     let i = prompt("son kiriting")
+//     if (i > 100 || i === null){
+//     console.log(i);
+//     break
+//     }
+// }
+
+// ========================================
+
+// do while
+
+// do {
+//   let i = prompt("son kiriting");
+//   if (i > 100 || i === null) {
+//     console.log(i);
+//     break;
+//   }
+// } while (1);
+// let i = 0
+// do {
+//   console.log(i);
+//   i++
+
+// }while (1)
+
+// for (let i = 1; i <= 20; i++) {
+//   for (let j = 1; j <= 20; j++) {
+//     console.log(`${i} x ${j} = ${i * j}`);
+//   }
+//   console.log("========================");
+// }
+
+// label
+
+// for (let i = 1; i <= 20; i++) {
+//   for (let j = 1; j <= 20; j++) {
+    
+//     console.log(`${i} x ${j} = ${i * j}`);
+//   }
+//   console.log("========================");
+// }
--- Homework 4/index.js
+++ Homework 4/index.js
@@ -1,25 +1,25 @@
-// let number = 20;
+let number = 200;
 
-// for (let i = 2; i <= number; i++) {
-//   let tup= true;
+for (let i = 2; i <= number; i++) {
+  let tup= true;
 
-//   for (let bolnuvchi = 2; bolnuvchi < i; bolnuvchi++) {
-//     if (i % j === 0) {
-//       tup = false;
-//       break;
-//     }
-//   }
-
-//   if (tup) {
-//     console.log(i);
-//   }
-// }
-
-
-for( ;true; ){
-    let n = prompt("son kiriting")
-    if(n > 100 || n === null){
-        console.log(n);
-        break
+for (let bolnuvchi = 2; bolnuvchi < i; bolnuvchi++) {
+    if (i % bolnuvchi === 0) {
+      tup = false;
+      break;
     }
+  }
+
+  if (tup) {
+    console.log(i);
+  }
 }
+
+
+// for( ;true; ){
+//     let n = prompt("son kiriting")
+//     if(n > 100 || n === null){
+//         console.log(n);
+//         break
+//     }
+// }
