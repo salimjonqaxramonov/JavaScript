1dde678e5ac50d02bed8f3bf9efee828a896fc69
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-11-05 16:55:54 +0500

    11-dars

---
+++ 11-dars/index.js
@@ -0,0 +1,32 @@
+// let a = 20;
+// let b = 30;
+// function xisobla(){
+//     console.log(20+30);
+// }
+// xisobla();
+
+// //====================================
+
+// let xisobla2 = function() {
+//     console.log("Biz bugun Function mavzusini O'tmoqdamiz.");
+// };
+// xisobla2();
+
+// //========================================
+
+// let xisobla3 = () => {
+//     console.log("Oxford");
+// };
+// xisobla3();
+
+//====================================
+
+let a = 20;
+let b = 30;
+
+function xisobla() {
+    let sum = a + b;
+    return sum;
+}
+
+console.log(xisobla());
