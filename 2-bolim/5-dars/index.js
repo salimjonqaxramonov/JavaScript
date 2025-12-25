2e5a39fe54a1c1ef8b3b6784663502a46602778d
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-12-25 19:28:08 +0500

    lesson

---
+++ 2-bolim/5-dars/index.js
@@ -0,0 +1,81 @@
+// let arr = [
+//     {id: 1 , name:"Eshmat"},
+
+//     {id: 2 , name:"Ali"},
+
+//     {id: 3 , name:"Vali"}
+// ]
+
+//  let newArray = arr.filter((obj) => obj.name.includes("m"))
+
+// console.log(newArray);
+
+
+// console.log(arr);
+
+// let arr = [
+//     {id: 1 , name:"Eshmat",status:"ishchi"},
+
+//     {id: 2 , name:"Ali", status: "quruvchi"},
+
+//     {id: 3 , name:"Vali", status: "teacher"},
+
+//     {id: 4 , name:"Salim", status: "Biznesmen"},
+
+//     {id: 5 , name:"Toshmat", status: "bekorchi"}
+
+// ]
+
+// arr = arr.sort((a, b) => a.name.localeCompare(b.name))
+
+//  console.log(arr);
+
+// let textbooks = (['Algebra', 'History', 'Geometry', 'English'])
+
+// let sorter = (textbooks) => textbooks.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1)
+
+// console.log(textbooks);
+
+let people = [
+    { id: 1, firstName: "Eshmat", lastName: "Toshmatov", age: 20, status: "Talaba" },
+    { id: 2, firstName: "Gulmat", lastName: "Surmatov", age: 17, status: "O'quvchi" },
+    { id: 3, firstName: "Dilshod", lastName: "Karimov", age: 25, status: "Ishchi" },
+    { id: 4, firstName: "Malika", lastName: "Xoliqova", age: 30, status: "Uqituvchi" },
+    { id: 5, firstName: "Zafar", lastName: "Bozorov", age: 19, status: "Talaba" },
+    { id: 6, firstName: "Umida", lastName: "Raxmatova", age: 22, status: "Tadbirkor" },
+    { id: 7, firstName: "Rustam", lastName: "Ziyodov", age: 35, status: "Dasturchi" },
+    { id: 8, firstName: "Nodira", lastName: "Saidova", age: 28, status: "Muhandis" },
+    { id: 9, firstName: "Javlon", lastName: "Qurbonov", age: 24, status: "Frilanser" },
+    { id: 10, firstName: "Shahnoza", lastName: "Ergasheva", age: 18, status: "O'quvchi" }
+  ];
+// 1-misol oxshamadi
+//   let data = people.find((obj) => obj.id === 5);
+// if (data) {
+//   data === {id: 5, firstName: "Ali", lastName: "Valiyev", age: 25, status: "Dasturchi"}
+// }
+// console.log(people);
+
+//====================
+
+ // 2-misol
+//  let newArray = people.filter((obj) => obj.lastName.includes("va"))
+//    console.log(newArray);
+
+//=======================
+
+// 3-misol
+// people = people.filter((obj) => obj.id !== 4 && obj.id !== 9);
+// console.log(people);
+ //=======================
+
+ //4--misol
+//   let id = people.find((obj) => obj.id === 7);
+// if (id) {
+//   id.firstName = "Gulbashakar";
+// }
+// console.log(people);
+//======================
+
+  // 5-misol
+// people = people.sort((a, b) => a.lastName.localeCompare(b.lastName))
+// console.log(people);
